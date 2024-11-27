0b9a56e6063406dbd36822d45482d489b99d7b93
armand1205 <munteanuarmandshadow1205@gmail.com>
2024-11-27 22:38:05 +0200

    code-refactoring

--- script.js
+++ script.js
@@ -1,84 +1,42 @@
 "use strict";
-
-/* console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value); */
-
-// SecretNumber declaration
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-// Stabilim scorul initial
 let score = 20;
-
-// Declaram Highscore
 let highscore = 0;
-
-// Event handler pt butonul de CHECK
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
-
-  // Afisare mesaj in functie de numar: daca este corect, mai mare sau mai mic
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "No number!";
-
+    displayMessage("⛔️ No number!");
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct number!";
-
-    // Schimbare culoare BACKGROUND cand numarul este CORECT
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
-
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
-
-    // When guess is to high
-  } else if (guess > secretNumber) {
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too high!";
-
-      // Scade din scor daca numarul nu e corect
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
+      displayMessage("💥 You lost the game!");
       document.querySelector(".score").textContent = 0;
-      // Schimbare culoare BACKGROUND cand numarul este GRESIT
-      document.querySelector("body").style.backgroundColor = "#c21807";
-    }
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too low!";
-
-      // Scade din scor daca numarul nu e corect
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-
-      // Schimbare culoare BACKGROUND cand numarul este GRESIT
-      document.querySelector("body").style.backgroundColor = "#c21807";
     }
   }
 });
-
-// Event handler pt butonul de AGAIN!
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
